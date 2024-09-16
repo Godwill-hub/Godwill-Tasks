2e280f575bac4a9901ca69ac69e7ae5221646ca8
Godwill-hub <patrickgodwill7@gmail.com>
2024-09-16 09:09:15 +0300

    Update movieItem.java

--- src/movie/movieItem.java
+++ src/movie/movieItem.java
@@ -1,20 +1,20 @@
 package movie;
 
 public class movieItem{
-    private String title;//this variable stores the title of the movie
-    private String author;//this variable stores the author of the movie
+    private String title;
+    private String author;
 
-    protected movieItem(String title,String author){//Here we have created a protected constructor to initialize the title and author
+    protected movieItem(String title,String author){
         this.title=title;
         this.author=author;
     }
-    public String getTitle(){// this method returns the title of the movie
+    public String getTitle(){
         return title;
     }
-    public String getAuthor(){// this method returns the author of the movie
+    public String getAuthor(){
         return author;
     }
-    void printDetails(){//this method helps us print the title and author of the movie
+    void printDetails(){
         System.out.println("Title:"+title+",Author:"+author);
     }
 }
